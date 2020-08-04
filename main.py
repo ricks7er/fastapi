0dfe46ef4a1666bfdfbb24ebca37ea9b3661c132
Jose <jrortegar@gmail.com>
2020-08-04 11:59:36 -0500

    second commit

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from enum import Enum
-from fastapi import FastAPI
+from typing import Optional
 
-class ModelName(str, Enum):
-    alexnet="alexnet"
-    resnet="resnet"
-    lenet="lenet"
+from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name==ModelName.alexnet:
-        return {"model_name":model_name,"message":"Deep Learning FTW!"}
-    if model_name==ModelName.lenet:
-        return {"model_name":model_name,"message":"LeCNN all the images"}
-    return {"model_name":model_name,"message":"Have some residuals"}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id:int, item_id:str,q:Optional[str]=None,short:bool=False):
+    item={"item_id":item_id,"owner_id":user_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update(
+            {"description":"This is an amazing item with a long description"}
+        )
+    return item
